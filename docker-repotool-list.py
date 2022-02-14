4e9ba1b2c5a604dc1b7c8aa4a95cd43704c4c5b4
thedetukov <84131104+thedetukov@users.noreply.github.com>
2022-02-14 20:02:19 +0000

    Add files via upload

---
+++ docker-repotool-list.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+#
+import requests 
+from requests.auth import HTTPBasicAuth
+import os
+import sys
+import json
+
+
+def catalogs_repositories_tags_data():
+
+	docker_registry_api_url = os.environ.get('DOCKER_REGISTRY_API_URL', None)
+	if docker_registry_api_url is None:
+		print("A required environment variable 'DOCKER_REGISTRY_API_URL' is not set. Enter the desired URL.", file=sys.stderr)
+		sys.exit(1)
+
+	login_user= os.environ.get('LOGIN_USER', None)
+	if login_user is None:
+		print("A required environment variable 'LOGIN_USER' is not set. Enter the desired LOGIN.", file=sys.stderr)
+		sys.exit(2)
+	
+	password_user= os.environ.get('PASSWORD_USER', None)
+	if password_user is None:
+		print("A required environment variable 'PASSWORD_USER' is not set. Enter the desired PASSWORD.", file=sys.stderr)
+		sys.exit(3)
+
+	try:
+		credentials = HTTPBasicAuth(login_user, password_user)
+		catalog_url = docker_registry_api_url + "/v2/_catalog"
+		catalog_response = requests.get(catalog_url, auth = credentials)
+		catalog_repositories_content = catalog_response.json()
+		mass_catalog_repositories_content = catalog_repositories_content['repositories']
+		
+		for tags in mass_catalog_repositories_content:
+			data_tags_url= docker_registry_api_url + "/v2/%s/tags/list" % tags
+			tags_data_response = requests.get(data_tags_url, auth = credentials)
+			data_tags_repositories_content = tags_data_response.json()
+			catalogs_name = data_tags_repositories_content['name']
+			
+			for name_tags in data_tags_repositories_content['tags']:
+				time_url = docker_registry_api_url + "/v2/%s/manifests/%s" % (catalogs_name, name_tags)  
+				time_response = requests.get(time_url, auth = credentials)
+				time_repositories_content = time_response.json()
+				time_repositories = time_repositories_content['history']
+				dict_time_repositories = time_repositories[0]
+				dict_tegs_repositories = dict_time_repositories['v1Compatibility']
+				data_time_repositories = json.loads(dict_tegs_repositories)
+				tags_date = data_time_repositories['created']
+				print(catalogs_name+'/'+name_tags, tags_date)
+		
+	except Exception as ex:
+		print("Attempt failure. {0}".format(ex), file=sys.stderr)
+
+catalogs_repositories_tags_data()
+
+
+
+
+
+
+
+	
